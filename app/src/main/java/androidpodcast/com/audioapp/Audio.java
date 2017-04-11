5219e065d8adb20bd26ee400542f46f4d96a227c
Neel Patel <patelneel55@gmail.com>
2017-04-11 18:31:37 -0500

    New Update! Media Player Service 4-11-2017

---
+++ app/src/main/java/androidpodcast/com/audioapp/Audio.java
@@ -0,0 +1,67 @@
+package androidpodcast.com.audioapp;
+
+import java.io.Serializable;
+
+/**
+ * Created by Aatu on 4/11/2017.
+ */
+
+public class Audio implements Serializable
+{
+    private String data;
+    private String title;
+    private String album;
+    private String artist;
+
+    public Audio(String data, String title, String album, String artist)
+    {
+        this.data = data;
+        this.title = title;
+        this.album = album;
+        this.artist = artist;
+    }
+
+    ////////////////////////////*Getters*/////////////////////////////////////
+
+    public String getData()
+    {
+        return data;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAlbum()
+    {
+        return album;
+    }
+
+    public String getArtist()
+    {
+        return artist;
+    }
+
+    ////////////////////////////*Setters*/////////////////////////////////////
+
+    public void setData(String data)
+    {
+        this.data = data;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public void setAlbum(String album)
+    {
+        this.album = album;
+    }
+
+    public void setArtist(String artist)
+    {
+        this.artist = artist;
+    }
+}
